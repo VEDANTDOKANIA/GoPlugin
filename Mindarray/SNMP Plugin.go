6bc96599d1b4e9edf4d6f21a0c750fdaafe8e522
Vedant Dokania <61377008+VEDANTDOKANIA@users.noreply.github.com>
2022-02-23 10:38:46 +0530

    After SNMP Plugin

--- Mindarray/SNMP Plugin.go
+++ Mindarray/SNMP Plugin.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	g "github.com/gosnmp/gosnmp"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -65,91 +66,135 @@ func snmpData(JsonData map[string]interface{}, c chan string) {
 	}
 
 	interfaceCount := (configmap["Total_Interface"]).(int)
-
-	var interfacelist []map[string]interface{}
-	interfaceindex := params.BulkWalk("1.3.6.1.2.1.2.2.1.1", WalkFunction)
+	interfaceindex := params.Walk("1.3.6.1.2.1.2.2.1.1", WalkFunction)
 	if interfaceindex != nil {
 
 	}
+	var interfaceoid []string
+	var description string = "1.3.6.1.2.1.2.2.1.2."
+	var Interfacetype string = "1.3.6.1.2.1.2.2.1.3."
+	var operatingstatus string = "1.3.6.1.2.1.2.2.1.8."
+	var adminstatus string = "1.3.6.1.2.1.2.2.1.7."
+	var inerrors string = "1.3.6.1.2.1.2.2.1.14."
+	var outerrors string = "1.3.6.1.2.1.2.2.1.20."
 
 	for i := 0; i < interfaceCount; i++ {
-		tempmap := make(map[string]interface{})
-		tempmap["Interface_Index"], _ = strconv.ParseInt(interfaceoutput[i], 10, 64)
-		interfacelist = append(interfacelist, tempmap)
+		Index_value, _ := strconv.ParseInt(interfaceoutput[i], 10, 64)
+		descriptionoid := description + strconv.Itoa(int(Index_value))
+		interfaceoid = append(interfaceoid, descriptionoid)
+		typeoid := Interfacetype + strconv.Itoa(int(Index_value))
+		interfaceoid = append(interfaceoid, typeoid)
+		operatingstatusoid := operatingstatus + strconv.Itoa(int(Index_value))
+		interfaceoid = append(interfaceoid, operatingstatusoid)
+		adminstatusoid := adminstatus + strconv.Itoa(int(Index_value))
+		interfaceoid = append(interfaceoid, adminstatusoid)
+		inerrornoid := inerrors + strconv.Itoa(int(Index_value))
+		interfaceoid = append(interfaceoid, inerrornoid)
+		outerroroid := outerrors + strconv.Itoa(int(Index_value))
+		interfaceoid = append(interfaceoid, outerroroid)
 	}
-	interfaceoutput = nil
+	var interfacelist []map[string]interface{}
+	var startIndex = 0
+	var endIndex = 60
+	var flag = 1
+	var inflag = 1
+	for flag == 1 {
+		if inflag == 0 {
+			flag = 0
+		}
+		InterfaceResult, err := params.Get(interfaceoid[startIndex:endIndex])
+		if err != nil {
+			fmt.Println(err)
+		}
+		var count int = 0
+		interfaceMap := make(map[string]interface{})
+		for _, variable := range InterfaceResult.Variables {
 
-	interfacedescription := params.BulkWalk("1.3.6.1.2.1.2.2.1.2", WalkFunction)
-	if interfacedescription != nil {
+			VariableName := strings.SplitAfter(variable.Name, ".1.3.6.1.2.1.2.2.1.")
+			RootOid := VariableName[0] + strings.Split(VariableName[1], ".")[0] + "."
+			//VariableName := variable.Name[:len(variable.Name)-1]
+			switch RootOid {
+			case ".1.3.6.1.2.1.2.2.1.2.":
+				descriptionValue := string(variable.Value.([]byte))
+				interfaceMap["Interface.Description"] = descriptionValue
+				interfaceMap["Interface.Index"], _ = strconv.ParseInt(strings.Split(VariableName[1], ".")[1], 10, 64)
+				count++
+			case ".1.3.6.1.2.1.2.2.1.3.":
+				var typevalue string
+				switch (variable.Value).(int) {
+				case 6:
+					typevalue = "ethernetCsmacd"
+				case 1:
+					typevalue = "other"
+				case 135:
+					typevalue = "l2vlan"
+				case 53:
+					typevalue = "propVirtual"
+				case 24:
+					typevalue = "softwareLoopback"
+				case 131:
+					typevalue = "tunnel"
+				}
+				interfaceMap["Interface.Type"] = typevalue
+				count++
+			case ".1.3.6.1.2.1.2.2.1.8.":
+				var operatingvalue string
+				if variable.Value.(int) == 1 {
+					operatingvalue = "Up"
+				}
+				if variable.Value.(int) == 2 {
+					operatingvalue = "Down"
+				}
+				interfaceMap["Interface.Operating_Status"] = operatingvalue
+				count++
+			case ".1.3.6.1.2.1.2.2.1.7.":
+				var Adminvalue string
+				if variable.Value.(int) == 1 {
+					Adminvalue = "Up"
+				}
+				if variable.Value.(int) == 2 {
+					Adminvalue = "Down"
+				}
+				interfaceMap["Interface.Admin_Status"] = Adminvalue
+				count++
+			case ".1.3.6.1.2.1.2.2.1.14.":
+				if variable.Value == nil {
+					interfaceMap["Interface.InError"] = ""
+				} else {
+					interfaceMap["Interface.InError"] = variable.Value
+				}
+				count++
+			case ".1.3.6.1.2.1.2.2.1.20.":
+				if (variable.Value) == nil {
+					interfaceMap["Interface.OutError"] = ""
+				} else {
+					interfaceMap["Interface.OutError"] = variable.Value
+				}
 
+				count++
+			}
+			if count == 6 {
+				interfacelist = append(interfacelist, interfaceMap)
+				interfaceMap = make(map[string]interface{})
+				count = 0
+			}
+		}
+		//fmt.Println(interfacelist)
+		startIndex = endIndex
+		endIndex = endIndex + 60
+		if endIndex > len(interfaceoid) {
+			endIndex = len(interfaceoid)
+			inflag = 0
+		}
 	}
-
-	for i := 0; i < interfaceCount; i++ {
-		tempmap := make(map[string]interface{})
-		tempmap = interfacelist[i]
-		tempmap["Interface_Description"] = interfaceoutput[i]
-		interfacelist[i] = tempmap
-	}
-	interfaceoutput = nil
-
-	interfaceoutput = nil
-	interfacestatus := params.BulkWalk("1.3.6.1.2.1.2.2.1.7", WalkFunction)
-	if interfacestatus != nil {
-
-	}
-
-	for i := 0; i < interfaceCount; i++ {
-		tempmap := make(map[string]interface{})
-		tempmap = interfacelist[i]
-		status_value, _ := strconv.ParseInt(interfaceoutput[i], 10, 64)
-		if status_value == 1 {
-			tempmap["Interface_Status"] = "Up"
-			interfacelist[i] = tempmap
-		}
-
-		if status_value == 2 {
-			tempmap["Interface_Status"] = "Down"
-			interfacelist[i] = tempmap
-		}
-	}
-	interfaceoutput = nil
-	interfacetype := params.BulkWalk("1.3.6.1.2.1.2.2.1.3", WalkFunction)
-	if interfacetype != nil {
-
-	}
-
-	for i := 0; i < interfaceCount; i++ {
-		tempmap := make(map[string]interface{})
-		tempmap = interfacelist[i]
-		type_value, _ := strconv.ParseInt(interfaceoutput[i], 10, 64)
-		if type_value == 6 {
-			tempmap["Interface_Type"] = "ethernetCsmacd"
-			interfacelist[i] = tempmap
-		}
-		if type_value == 135 {
-			tempmap["Interface_Type"] = "l2vlan"
-			interfacelist[i] = tempmap
-		}
-		if type_value == 53 {
-			tempmap["Interface_Type"] = "propVirtual"
-			interfacelist[i] = tempmap
-		}
-		if type_value == 24 {
-			tempmap["Interface_Type"] = "softwareLoopback"
-			interfacelist[i] = tempmap
-		}
-		if type_value == 131 {
-			tempmap["Interface_Type"] = "tunnel "
-			interfacelist[i] = tempmap
-		}
-
-	}
-	interfaceoutput = nil
-
+	//fmt.Println(interfacelist)
 	configmap["Interface"] = interfacelist
+	configmap["IP-Address"] = JsonData["IP address"].(string)
 
-	output, _ := json.Marshal(configmap)
-
+	output, err := json.Marshal(configmap)
+	if err != nil {
+		fmt.Println(err)
+	}
 	c <- string(output)
 
 }
